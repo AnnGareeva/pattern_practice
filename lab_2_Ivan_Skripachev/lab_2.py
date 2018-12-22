1469224902aa8d9d2ffdf325a7374d88708e5419
Ivan Skripachev <skripachevivan@gmail.com>
2018-12-22 16:48:13 +0300

    Added checking weight sum and updated tests

--- lab_2_Ivan_Skripachev/lab_2.py
+++ lab_2_Ivan_Skripachev/lab_2.py
@@ -1,44 +1,41 @@
 import os
+import sys
 import argparse
 
-MAX_LEN_LINE = 39
+MAX_LEN_LINE = 35
 NUM_LINES_IN_DIGIT = 4
-MAX_NUM_DIGITS = 10
+NUM_DIGITS = 9
 DIGIT_WIDTH = 3
+WEIGHT_COEF = 11
 
 parser = argparse.ArgumentParser(add_help=True)
 parser.add_argument('-i', action='store', dest='input_file',  help='Path to file with input digits')
 parser.add_argument('-o', action='store', dest='output_file', help='Path to file for result')
 args = parser.parse_args()
 
-digits_dict = {'._.|.||_|...': '0',
-               '.....|..|...': '1',
-               '._.._||_....': '2',
-               '._.._|._|...': '3',
-               '...|_|..|...': '4',
-               '._.|_.._|...': '5',
-               '._.|_.|_|...': '6',
-               '._...|..|...': '7',
-               '._.|_||_|...': '8',
-               '._.|_|._|...': '9'}
+digits_dict = {' _ | ||_|   ': '0',
+               '     |  |   ': '1',
+               ' _  _||_    ': '2',
+               ' _  _| _|   ': '3',
+               '   |_|  |   ': '4',
+               ' _ |_  _|   ': '5',
+               ' _ |_ |_|   ': '6',
+               ' _   |  |   ': '7',
+               ' _ |_||_|   ': '8',
+               ' _ |_| _|   ': '9'}
 
 
 def read_console():
     result = []
-    
-    stop_input = False
-    while True:
-        for i in range(NUM_LINES_IN_DIGIT):
-            input_data = raw_input()
-            if len(input_data) > MAX_LEN_LINE:
-                raise SystemError('Incorrect input')
-            elif len(input_data) < MAX_LEN_LINE:
-                stop_input = True
-            result.append(input_data)
-        if stop_input:
-            break
-    
+    for line in sys.stdin:
+        result.append(line)
+    if len(result) % NUM_LINES_IN_DIGIT != 0:
+        raise SystemError('Incorrect number of lines in console')
     return result
+
+def write_console(codes):
+    for code in codes:
+        print(code)
 
 
 def read_file(file_name):
@@ -49,50 +46,50 @@ def read_file(file_name):
         file_data = f.readlines()
         
         if len(file_data) % NUM_LINES_IN_DIGIT != 0:
-            raise SystemError('Incorrect file')
-
-        for line in file_data:
-            if len(line.rstrip()) > MAX_LEN_LINE:
-                raise SystemError('Incorrect file')
+            raise SystemError('Incorrect number of lines in file')
 
     return file_data
 
 
 def write_file(file_name, data):
     with open(file_name, 'w') as f:
-        f.writelines(data)
+        for row in data:
+            f.write(row + '\n')
 
 
 def convert_digits(raw_digits_list):
     result = ''
-
     for digit in raw_digits_list:
-        if digit:
-            key = ('').join(digit)
-            if key not in digits_dict:
-                raise KeyError('Incorrect symbols in input digits')
-            result += digits_dict[key]
-
+        key = ('').join(digit)
+        result += digits_dict.get(key, '?')
     return result
 
 
 def procces_file_data(file_data):
-    result = ''
+    result = []
 
-    for digit_line_idx in range(0, len(file_data) / NUM_LINES_IN_DIGIT):
-        values = [[] for i in range(MAX_NUM_DIGITS)]
+    for digit_line_idx in range(len(file_data) / NUM_LINES_IN_DIGIT):
+        values = [[] for i in range(NUM_DIGITS)]
         for row_idx in range(NUM_LINES_IN_DIGIT):
-            s = file_data[digit_line_idx * NUM_LINES_IN_DIGIT + row_idx].rstrip()
-            for i, col_idx in enumerate(range(0, len(s), DIGIT_WIDTH+1)):
-                values[i] += s[col_idx:col_idx+DIGIT_WIDTH]
+            row_data = file_data[digit_line_idx * NUM_LINES_IN_DIGIT + row_idx].rstrip('\r\n')
+            for i, col_idx in enumerate(range(0, len(row_data), DIGIT_WIDTH+1)):
+                values[i] += row_data[col_idx:col_idx+DIGIT_WIDTH]
         
         conv_digit = convert_digits(values)
-        result += conv_digit
-
-        if len(conv_digit) < MAX_NUM_DIGITS:
-            break
+        result.append(conv_digit)
     
     return result
+
+def is_corrected(code):
+    if '?' in code:
+        return code + ' ILL'
+    
+    weight_sum = 0
+    for i in range(len(code)):
+        weight_sum += int(code[i]) * (len(code) - i)
+
+    return code + ' ERROR' if weight_sum % WEIGHT_COEF else code
+    
 
 def main():
     if args.input_file:
@@ -102,10 +99,13 @@ def main():
 
     res = procces_file_data(file_data)
 
+    for i in range(len(res)):
+        res[i] = is_corrected(res[i])
+
     if args.output_file:
         write_file(args.output_file, res)
     else:
-        print(res)
+        write_console(res)
     
 if __name__ == '__main__':
     main()
--- lab_2_Ivan_Skripachev/test_lab_2.py
+++ lab_2_Ivan_Skripachev/test_lab_2.py
@@ -6,60 +6,75 @@ from subprocess import Popen, PIPE
 class DigitConverterTest(unittest.TestCase):
     def setUp(self):
         self.test_correct_input_list = [
-            '._. ... ._. ._. ... ._. ._. ._.\n'
-            '|.| ..| ._| ._| |_| |_. |_. ..|\n'
-            '|_| ..| |_. ._| ..| ._| |_| ..|\n'
-            '... ... ... ... ... ... ... ...',
+            ' _       _   _       _   _   _   _ \n'
+            '| |   |  _|  _| |_| |_  |_    | |_|\n'
+            '|_|   | |_   _|   |  _| |_|   | |_|\n'
+            '                                   ',
         
-            '._. ... ._. ._. ... ._. ._. ._. ._. ._.\n'
-            '|.| ..| ._| ._| |_| |_. |_. ..| |_| |_|\n'
-            '|_| ..| |_. ._| ..| ._| |_| ..| |_| ._|\n'
-            '... ... ... ... ... ... ... ... ... ...\n'
-            '._. ... ._. ._. ... ._. ._. ._. ._.\n'
-            '|.| ..| ._| ._| |_| |_. |_. ..| |_|\n'
-            '|_| ..| |_. ._| ..| ._| |_| ..| |_|\n'
-            '... ... ... ... ... ... ... ... ...']
+            '     _   _       _   _   _   _   _ \n'
+            '  |  _|  _| |_| |_  |_    | |_| |_|\n'
+            '  | |_   _|   |  _| |_|   | |_| |_|\n'
+            '                                   \n'
+            ' _   _   _   _   _   _   _   _     \n'
+            '| | | | | | | | | | | | | | |_    |\n'
+            '|_| |_| |_| |_| |_| |_| |_|  _|   |\n'
+            '                                   \n'
+            ' _   _   _   _   _   _   _   _   _ \n'
+            '|_| | | | | |_  | | | | | |  _| | |\n'
+            ' _| |_| |_| |_| |_| |_| |_| |_  |_|\n'
+            '                                   ']
 
-        self.expected_result = [
-            '01234567',
-            '0123456789012345678'
+        self.expected_correct_result = [
+            '012345678 ERROR',
+            '123456788 ERROR\n'
+            '000000051\n'
+            '900600020'
         ]
 
         self.test_incorrect_input_list = [
-            '._. ... ._. ._. ... ._. ._. ._. ._. ._. ._.\n'
-            '|.| ..| ._| ._| |_| |_. |_. ..| |_| |_| |.|\n'
-            '|_| ..| |_. ._| ..| ._| |_| ..| |_| ._| |_|\n'
-            '... ... ... ... ... ... ... ... ... ... ...',
+            ' _       _   _       _   _   _   _ \n'
+            '| |      _|  _|  _| |_  |     | |_|\n'
+            '|_|   | |_   _|   |  _| |_|   | |_|\n'
+            '                                   ',
 
-            '._. ... ._. ._. ... ._. ._. ._.\n'
-            '|.| ..| ._| ._| |_| |_. |_. |||\n'
-            '|_| ..| |_. ._| ..| ._| |_| |||\n'
-            '... ... ... ... ... ... ... ...',
+            ' _       _   _       _   _       _ \n'
+            '| |   |  _|  _|     |_  |_      |_|\n'
+            '|_|   | |_   _|      _| |_|     |_|\n'
+            '                                   ',
 
-            '._. ... ._. ._. ...\n'
-            '|aa ..| ._| ._| |_|\n'
-            '|_| ..| |_. ._| ..|\n'
-            '... ... ... ...',
+            ' _       _   _       _   _   _   _ \n'
+            '|aa   |  _\  BB |_| 178 |_    | |_|\n'
+            '|_|   | |_   _|   |  _| *_*   | |_|\n'
+            '                                   '
+        ]
+
+        self.expected_incorrect_result = [
+            '0?23?5?78 ILL',
+            '0123?56?8 ILL',
+            '?1??4??78 ILL'
         ]
 
         self.test_correct_input_files = {
-            'digits_example_1.txt' : '0123456789012345678',
-            'digits_example_2.txt' : '012345678901234'
+            'digits_example_1.txt' : '012345678 ERROR\n'
+                                     '123456789',
+            'digits_example_2.txt' : '900600020'
         }
 
-        self.test_incorrect_input_files = [
-            'digits_incorrect_example_1.txt',
-            'digits_incorrect_example_2.txt',
-            'digits_incorrect_example_3.txt',
-        ]
+        self.test_incorrect_input_files = {
+            'digits_incorrect_example_1.txt' : '?0000?0?1 ILL',
+            'digits_incorrect_example_2.txt' : '01?345678 ILL',
+            'digits_incorrect_example_3.txt' : '0?2?456?? ILL'
+        }
 
     def test_correct_input_console(self):
+        # Input console | Output to console
         command = ["python", "lab_2.py"]
         for i in range(len(self.test_correct_input_list)):
             p = Popen(command, stdout=PIPE, stdin=PIPE)
             output = p.communicate(input=self.test_correct_input_list[i])[0]
-            self.assertEqual(output.rstrip(), self.expected_result[i])
+            self.assertEqual(output.rstrip(), self.expected_correct_result[i])
 
+        # Input console | Output to file
         out_file_name = 'test_correct_input_console.txt'
         command = ["python", "lab_2.py", '-o', out_file_name]
         for i in range(len(self.test_correct_input_list)):
@@ -67,23 +82,26 @@ class DigitConverterTest(unittest.TestCase):
             output = p.communicate(input=self.test_correct_input_list[i])[0]
             self.assertTrue(os.path.exists(out_file_name))
             with open(out_file_name, 'r') as f:
-                output = f.readline()
-            self.assertEqual(output.rstrip(), self.expected_result[i])
+                output = f.read()
+            self.assertEqual(output.rstrip(), self.expected_correct_result[i])
     
     def test_incorrect_input_console(self):
+        # Input console incorrect
         command = ["python", "lab_2.py"]
         for i in range(len(self.test_incorrect_input_list)):
             p = Popen(command, stdout=PIPE, stdin=PIPE)
             output = p.communicate(input=self.test_incorrect_input_list[i])[0]
-            self.assertNotEqual(p.returncode, 0)
+            self.assertEqual(output.rstrip(), self.expected_incorrect_result[i])
 
     def test_correct_input_file(self):
+        # Input file | Output to console
         for file_name, result in self.test_correct_input_files.items():
             command = ["python", "lab_2.py", "-i", file_name]
             p = Popen(command, stdout=PIPE, stdin=PIPE)
             output = p.communicate()[0]
             self.assertEqual(output.rstrip(), result)
 
+        # Input file | Output to file
         out_file_name = 'test_correct_input_file.txt'
         for file_name, result in self.test_correct_input_files.items():
             command = ["python", "lab_2.py", "-i", file_name, "-o", out_file_name]
@@ -91,15 +109,16 @@ class DigitConverterTest(unittest.TestCase):
             p.communicate()
             self.assertTrue(os.path.exists(out_file_name))
             with open(out_file_name, 'r') as f:
-                output = f.readline()
+                output = f.read()
             self.assertEqual(output.rstrip(), result)
 
     def test_incorrect_input_file(self):
-        for file_name in self.test_incorrect_input_files:
+        # Input file incorrect
+        for file_name, result in self.test_incorrect_input_files.items():
             command = ["python", "lab_2.py", "-i", file_name]
             p = Popen(command, stdout=PIPE, stdin=PIPE)
             output = p.communicate()[0]
-            self.assertNotEqual(p.returncode, 0)
+            self.assertEqual(output.rstrip(), result)
 
 if __name__ == '__main__':
     unittest.main()
